b5a60a24c3fc086c599403e4c2a2315ff66de7f6
clojosh <joshua.lee@clo3d.com>
2025-04-15 18:30:58 -0700

    Finished api_list

--- src/api.py
+++ src/api.py
@@ -2,6 +2,7 @@ import json
 import os
 import re
 import sys
+from datetime import datetime, timezone
 from pathlib import Path
 
 import questionary
@@ -12,72 +13,77 @@ from tools.azure import Azure
 from tools.misc import trim_tokens
 
 
-class CLOAPI:
+class API:
     def __init__(self, azure: Azure):
         self.azure = azure
-        self.clo_api_path = os.path.join("data", azure.brand, "clo_api")
+
+        if not os.path.exists(os.path.join("data", azure.brand, "api")):
+            os.makedirs(os.path.join("data", azure.brand, "api"), exist_ok=True)
+
+        self.api_path = os.path.join("data", azure.brand, "api")
+
+        if azure.brand == "md":
+            self.base_url = "https://developer.marvelousdesigner.com/"
+        else:
+            self.base_url = f"https://developer.{azure.brand}.com/"
 
     def parse_api_docs(self):
         """
         Parse the API documentation files and save them as JSON files.
         """
+        url = self.base_url + "_sources/list.rst.txt"
+        response = requests.get(url)
 
-        # Iterate over the API documentation files in the directory
-        for api_doc in os.listdir(os.path.join(self.clo_api_path, "api_list", "original_docs")):
-            docs = []
+        api_text = response.text
 
-            # Open the API documentation file
-            with open(os.path.join(self.clo_api_path, "api_list", "original_docs", api_doc), "r", encoding="utf-8") as f:
-                func_objects = json.load(f)
+        api_docs = []
+        matches = [(m.start(0), m.end(0)) for m in re.finditer(r".*_API\n", api_text)]
+        for i in range(len(matches)):
+            if i + 1 == len(matches):
+                doc = api_text[matches[i][0] :].split("********")
+            else:
+                doc = api_text[matches[i][0] : matches[i + 1][0]].split("***********")
 
-                # Iterate over the functions in the API documentation
-                for i, func in enumerate(func_objects):
-                    # Trim the function documentation
-                    content = trim_tokens(func_objects[func]["Doc"])
+            api_docs.append(
+                {
+                    "ArticleId": shortuuid.uuid(),
+                    "Source": url,
+                    "Title": doc[0].strip(),
+                    "Content": doc[1],
+                    "ContentDescription": self.azure.openai_helper.create_webpage_description(doc[1]),
+                    "CreatedAt": datetime.now(tz=timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"),
+                    "YoutubeLinks": [],
+                }
+            )
 
-                    # Format the function documentation to include the arguments and return type
-                    regex = r"{0}\(.*?\)".format(func)
-                    content = re.sub(regex, func + "(" + func_objects[func]["Args"] + ") -> " + func_objects[func]["ReturnType"], content)
+        if not os.path.exists(os.path.join(self.api_path, "api_list")):
+            os.makedirs(os.path.join(self.api_path, "api_list"), exist_ok=True)
 
-                    # Create a dictionary representing the parsed API documentation
-                    docs.append(
-                        {
-                            "ArticleId": shortuuid.uuid(),  # Generate a unique ID for the document
-                            "Source": "https://developer.clo3d.com/list.html",  # Specify the source URL
-                            "Title": func + " API",  # Format the title as the function name followed by "API"
-                            "Content": content,  # Use the trimmed function documentation
-                            "Labels": [],  # Initialize an empty list for labels
-                            "YoutubeLinks": [],  # Initialize an empty list for YouTube links
-                        }
-                    )
+        with open(os.path.join(self.api_path, "api_list", "api_docs.json"), "w+", encoding="utf-8") as f:
+            json.dump(api_docs, f, indent=4)
 
-                # Save the parsed API documentation as a JSON file
-                with open(os.path.join(self.clo_api_path, "api_list", api_doc), "w+", encoding="utf-8") as f:
-                    json.dump(docs, f, indent=4)
-
-    def parse_environment_setup_build_page(self):
+    def parse_environment_setup_build(self):
         """
         Parse the content of the "Environment Setup & Build" page and save it as a JSON file.
         """
 
         # Retrieve the content of the "Environment Setup & Build" page
-        url = "https://developer.clo3d.com/_sources/environment.rst.txt"
+        url = self.base_url + "_sources/environment.rst.txt"
         response = requests.get(url)
 
         # Create a dictionary with the article information
         env_setup_build = [
             {
                 "ArticleId": shortuuid.uuid(),  # Generate a unique identifier
-                "Source": "https://developer.clo3d.com/environment.html",  # URL of the original source
+                "Source": self.base_url + "environment.html",  # URL of the original source
                 "Title": "Environment Setup & Build",  # Title of the article
                 "Content": response.text,  # Content of the article
-                "Labels": [],  # List of labels associated with the article
                 "YoutubeLinks": [],  # List of YouTube links associated with the article
             }
         ]
 
         # Create the directory if it doesn't exist
-        env_setup_build_path = os.path.join(self.clo_api_path, "env_setup_build")
+        env_setup_build_path = os.path.join(self.api_path, "env_setup_build")
         if not os.path.exists(env_setup_build_path):
             os.mkdir(os.path.dirname(env_setup_build_path), exist_ok=True)
 
@@ -85,13 +91,13 @@ class CLOAPI:
         with open(os.path.join(env_setup_build_path, "env_setup_build.json"), "w+", encoding="utf-8") as f:
             json.dump(env_setup_build, f, indent=4)
 
-    def parse_api_scenario_page(self):
+    def parse_api_scenario(self):
         """
         Parse the content of the "API Scenario" page and save it as a JSON file.
         """
 
         # Retrieve the content of the "Environment Setup & Build" page
-        url = "https://developer.clo3d.com/_sources/scenario.rst.txt"
+        url = self.base_url + "_sources/scenario.rst.txt"
         response = requests.get(url)
 
         code_block = response.text.split("\n|\n|\n|\n\n\n")
@@ -122,7 +128,7 @@ class CLOAPI:
             )
 
         # Create the directory if it doesn't exist
-        env_setup_build_path = os.path.join(self.clo_api_path, "api_scenario")
+        env_setup_build_path = os.path.join(self.api_path, "api_scenario")
         if not os.path.exists(env_setup_build_path):
             os.makedirs(env_setup_build_path, exist_ok=True)
 
@@ -130,20 +136,20 @@ class CLOAPI:
         with open(os.path.join(env_setup_build_path, "api_scenario.json"), "w+", encoding="utf-8") as f:
             json.dump(code_block_dump, f, indent=4)
 
-    def parse_api_option_type_page(self):
+    def parse_api_option_type(self):
         """
         Parse the content of the "API Option & Type" page and save it as a JSON file.
         """
 
         # Retrieve the content of the "Environment Setup & Build" page
-        url = "https://developer.clo3d.com/_sources/optiontype.rst.txt"
+        url = self.base_url + "_sources/optiontype.rst.txt"
         response = requests.get(url)
 
         # Create a dictionary with the article information
         env_setup_build = [
             {
                 "ArticleId": shortuuid.uuid(),  # Generate a unique identifier
-                "Source": "https://developer.clo3d.com/optiontype.html",  # URL of the original source
+                "Source": self.base_url + "optiontype.html",  # URL of the original source
                 "Title": "API Option & Type",  # Title of the article
                 "Content": response.text,  # Content of the article
                 "Labels": [],  # List of labels associated with the article
@@ -152,7 +158,7 @@ class CLOAPI:
         ]
 
         # Create the directory if it doesn't exist
-        env_setup_build_path = os.path.join(self.clo_api_path, "api_option_type")
+        env_setup_build_path = os.path.join(self.api_path, "api_option_type")
         if not os.path.exists(env_setup_build_path):
             os.makedirs(env_setup_build_path, exist_ok=True)
 
@@ -161,11 +167,11 @@ class CLOAPI:
             json.dump(env_setup_build, f, indent=4)
 
     def upload_documents(self):
-        for dir in os.listdir(os.path.join(self.clo_api_path)):
-            for files in os.listdir(os.path.join(self.clo_api_path, dir)):
+        for dir in os.listdir(os.path.join(self.api_path)):
+            for files in os.listdir(os.path.join(self.api_path, dir)):
                 if files.endswith(".json"):
-                    print("Uploading: " + os.path.join(self.clo_api_path, dir, files))
-                    with open(os.path.join(self.clo_api_path, dir, files), "r", encoding="utf-8") as f:
+                    print("Uploading: " + os.path.join(self.api_path, dir, files))
+                    with open(os.path.join(self.api_path, dir, files), "r", encoding="utf-8") as f:
                         documents = json.load(f)
 
                         for i, document in enumerate(documents):
@@ -176,10 +182,10 @@ class CLOAPI:
                         self.environment.search_client.upload_documents(documents)
 
     def delete_documents(self):
-        for dir in os.listdir(os.path.join(self.clo_api_path)):
-            for files in os.listdir(os.path.join(self.clo_api_path, dir)):
+        for dir in os.listdir(os.path.join(self.api_path)):
+            for files in os.listdir(os.path.join(self.api_path, dir)):
                 if files.endswith(".json"):
-                    with open(os.path.join(self.clo_api_path, dir, files), "r", encoding="utf-8") as f:
+                    with open(os.path.join(self.api_path, dir, files), "r", encoding="utf-8") as f:
                         documents = json.load(f)
 
                         for i, document in enumerate(documents):
@@ -190,7 +196,7 @@ class CLOAPI:
 
 if __name__ == "__main__":
     stage = questionary.select("Which stage?", choices=["prod", "dev"]).ask()
-    brand = questionary.select("Which brand?", choices=["clo3d", "closet", "clovf", "md"]).ask()
+    brand = questionary.select("Which brand?", choices=["clo3d", "md"]).ask()
     task = questionary.select(
         "What task?",
         choices=[
@@ -203,18 +209,16 @@ if __name__ == "__main__":
         ],
     ).ask()
 
-    clo_api = CLOAPI(Azure(stage, brand))
+    clo_api = API(Azure(stage, brand))
 
     if task == "Parse API Docs":
-        delete_previous_documents = questionary.select("Did you delete previous documents?", choices=["Yes", "No"]).ask()
-        if delete_previous_documents == "Yes":
-            clo_api.parse_api_docs()
+        clo_api.parse_api_docs()
     elif task == "Parse Environment Setup & Build":
-        clo_api.parse_environment_setup_build_page()
+        clo_api.parse_environment_setup_build()
     elif task == "Parse API Scenario":
-        clo_api.parse_api_scenario_page()
+        clo_api.parse_api_scenario()
     elif task == "Parse API Option & Type":
-        clo_api.parse_api_option_type_page()
+        clo_api.parse_api_option_type()
     elif task == "Upload Documents":
         clo_api.upload_documents()
     elif task == "Delete Documents":
